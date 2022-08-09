ef1b6e15716db989894cf2239c1498191aef0eb7
manisha sanjay chavan <manisharathod8975@gmail.com>
2022-08-09 06:21:56 -0700

    SMS-01: entity class created

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,43 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+@Id
+	private int stid;
+	private String stname;
+	private String stage;
+	private String stemail;
+	public int getStid() {
+		return stid;
+	}
+	public void setStid(int stid) {
+		this.stid = stid;
+	}
+	public String getStname() {
+		return stname;
+	}
+	public void setStname(String stname) {
+		this.stname = stname;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getStemail() {
+		return stemail;
+	}
+	public void setStemail(String stemail) {
+		this.stemail = stemail;
+	}
+	@Override
+	public String toString() {
+		return "Student [stid=" + stid + ", stname=" + stname + ", stage=" + stage + ", stemail=" + stemail + "]";
+	}
+	
+	
+}
